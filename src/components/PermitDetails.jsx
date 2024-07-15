e1f27b61663043e7373f222cdf8e847c95f24a41
ram <rprasanth285@gmail.com>
2024-07-15 15:23:05 -0400

    added bootstrap form classes

--- src/components/PermitDetails.jsx
+++ src/components/PermitDetails.jsx
@@ -43,92 +43,136 @@ const PermitDetails = () => {
                     </div>
                 </div>
             </section>
-            <br />
+
             <section id="userInfo">
                 <div className="container userInfo">
-                    <h1>Applicant Information</h1>
-                    <label htmlFor="userName"> Name of applicant</label>
-                    <input readOnly type="text" id="userName" value={permitDetail?.applicantName || '-'} />
-                    <br />
-                    <label htmlFor="address"> Address</label>
-                    <input readOnly type="text" id="address" value={permitDetail?.applicantContactDetails?.address || '-'} />
-                    <br />
-                    <label htmlFor="mail"> email address</label>
-                    <input readOnly type="email" id="mail" value={permitDetail?.applicantContactDetails?.email || '-'} />
-                    <br />
-                    <label htmlFor="phone"> Phone number</label>
-                    <input readOnly type="number" id="phone" value={permitDetail?.applicantContactDetails?.phone || '-'} />
-                    <br />
-                    <label htmlFor="contractor"> Contractor Name</label>
-                    <input readOnly type="text" id="contractor" value={permitDetail?.contractorContactDetails?.name || '-'} />
+                    <h2>Applicant Information</h2>
+                    <div class="form-group row">
+                        <label htmlFor="userName" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Name of applicant</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="userName" value={permitDetail?.applicantName || '-'} />
+                        </div>
+
+                        <label htmlFor="address" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Address</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="address" value={permitDetail?.applicantContactDetails?.address || '-'} />
+                        </div>
+
+                        <label htmlFor="mail" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> email address</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="email" id="mail" value={permitDetail?.applicantContactDetails?.email || '-'} />
+                        </div>
+
+                        <label htmlFor="phone" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Phone number</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="number" id="phone" value={permitDetail?.applicantContactDetails?.phone || '-'} />
+                        </div>
+
+                        <label htmlFor="contractor" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Contractor Name</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="contractor" value={permitDetail?.contractorContactDetails?.name || '-'} />
+                        </div>
+                    </div>
                 </div>
             </section>
-            <br />
+
             <div className="horizantal-line" />
-            <br />
+
             <section id="propertyInfo">
                 <div className="container">
-                    <h1>Property Information</h1>
-                    <label htmlFor="propertyAddress"> Property Address</label>
-                    <input readOnly type="text" id="propertyAddress" value={permitDetail?.propertyInfo?.propertyAddress || '-'} />
-                    <br />
-                    <label htmlFor="lot"> Parcel or lot number</label>
-                    <input readOnly type="number" id="lot" value={permitDetail?.propertyInfo?.lot || '-'} />
-                    <br />
-                    <label htmlFor="legalDescription"> Legal Description</label>
-                    <input readOnly type="text" id="legalDescription" value={permitDetail?.propertyInfo?.legalDesc || '-'} />
-                    <br />
-                    <label htmlFor="propertyOwner"> Property Owner</label>
-                    <input readOnly type="text" id="propertyOwnerphone" value={permitDetail?.propertyInfo?.propertyOwner || '-'} />
+                    <h2>Property Information</h2>
+                    <div class="form-group row">
+                        <label htmlFor="propertyAddress" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Property Address</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="propertyAddress" value={permitDetail?.propertyInfo?.propertyAddress || '-'} />
+                        </div>
+
+                        <label htmlFor="lot" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Parcel or lot number</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="number" id="lot" value={permitDetail?.propertyInfo?.lot || '-'} />
+                        </div>
+
+                        <label htmlFor="legalDescription" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Legal Description</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="legalDescription" value={permitDetail?.propertyInfo?.legalDesc || '-'} />
+                        </div>
+
+                        <label htmlFor="propertyOwner" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Property Owner</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="propertyOwnerphone" value={permitDetail?.propertyInfo?.propertyOwner || '-'} />
+                        </div>
+                    </div>
                 </div>
             </section>
-            <br />
+
             <div className="horizantal-line" />
-            <br />
+
             <section id="projectDesc">
                 <div className="container">
-                    <h1>Property Description</h1>
-                    <label htmlFor="constructionType"> Construction Type</label>
-                    <input readOnly type="text" id="constructionType" value={permitDetail?.propertyDesc?.type || '-'} />
-                    <br />
-                    <label htmlFor="scope"> Scope of work</label>
-                    <input readOnly type="text" id="scope" value={permitDetail?.propertyDesc?.scope || '-'} />
-                    <br />
-                    <label htmlFor="cost"> Project cost</label>
-                    <input readOnly type="number" id="cost" value={permitDetail?.propertyDesc?.estimation || '-'} />
-                    <br />
-                    <label htmlFor="startDate"> Estimated start Date</label>
-                    <input readOnly type="date" id="startDate" value={permitDetail?.propertyDesc?.projectStartDate || '-'} />
-                    <br />
-                    <label htmlFor="endDate"> Estimated end Date</label>
-                    <input readOnly type="date" id="endDate" value={permitDetail?.propertyDesc?.projectEndDate || '-'} />
+                    <h2>Property Description</h2>
+                    <div class="form-group row">
+                        <label htmlFor="constructionType" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Construction Type</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="constructionType" value={permitDetail?.propertyDesc?.type || '-'} />
+                        </div>
+
+                        <label htmlFor="scope" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Scope of work</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="scope" value={permitDetail?.propertyDesc?.scope || '-'} />
+                        </div>
+
+                        <label htmlFor="cost" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Project cost</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="number" id="cost" value={permitDetail?.propertyDesc?.estimation || '-'} />
+                        </div>
+
+                        <label htmlFor="startDate" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Estimated start Date</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="date" id="startDate" value={permitDetail?.propertyDesc?.projectStartDate || '-'} />
+                        </div>
+
+                        <label htmlFor="endDate" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Estimated end Date</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="date" id="endDate" value={permitDetail?.propertyDesc?.projectEndDate || '-'} />
+                        </div>
+                    </div>
                 </div>
             </section>
-            <br />
+
             <div className="horizantal-line" />
-            <br />
+
             <section id="types">
                 <div className="container">
-                    <h1>Permit Type</h1>
-                    <label htmlFor="permitType"> Type of permit</label>
-                    <input readOnly type="text" id="permitType" value={permitDetail?.permit?.type || '-'} />
-                    <br />
-                    <label htmlFor="applicationNumber"> Application Number</label>
-                    <input readOnly type="number" id="applicationNumber" value={permitDetail?.permit?.application || '-'} />
-                    <br />
-                    <label htmlFor="fee"> Permit fees</label>
-                    <input readOnly type="number" id="fee" value={permitDetail?.permit?.fees || '-'} />
-                    <br />
-                    <label htmlFor="paymentMethod"> Payment Method </label>
-                    <input readOnly type="text" id="paymentMethod" value={permitDetail?.permit?.method || '-'} />
+                    <h2>Permit Type</h2>
+                    <div class="form-group row">
+                        <label htmlFor="permitType" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Type of permit</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="permitType" value={permitDetail?.permit?.type || '-'} />
+                        </div>
+
+                        <label htmlFor="applicationNumber" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Application Number</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="number" id="applicationNumber" value={permitDetail?.permit?.application || '-'} />
+                        </div>
+
+                        <label htmlFor="fee" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Permit fees</label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="number" id="fee" value={permitDetail?.permit?.fees || '-'} />
+                        </div>
+
+                        <label htmlFor="paymentMethod" className="col-sm-2 col-md-4 col-lg-2 col-form-label"> Payment Method </label>
+                        <div class="col-sm-10 col-md-8 col-lg-10">
+                            <input readOnly type="text" id="paymentMethod" value={permitDetail?.permit?.method || '-'} />
+                        </div>
+                    </div>
                 </div>
             </section>
-            <br />
+
             <div className="horizantal-line" />
-            <br />
+
             <section id="docs">
                 <div className="container">
-                    <h1>Design Document</h1>
+                    <h2>Design Document</h2>
                     <p> please upload the design document here</p>
                     <input type="file" />
                 </div>
